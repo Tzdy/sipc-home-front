7222c892bde4ef094a2a6503af55ba9a26cc8f82
Tsdy <571219188@qq.com>
2020-11-01 20:51:57 +0800

    初次提交

---
+++ src/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <input type="text" v-model="text">
+      <div> {{ text}} </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: '',
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
